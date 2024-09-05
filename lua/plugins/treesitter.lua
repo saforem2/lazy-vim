d2ea8323caaaa76cd5c0565b1f77178aff41cad6
Sam Foreman <saforem2@gmail.com>
2024-09-04 20:41:00 -0500

    Update `lua/plugins/{astro,treesitter}.lua`

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,32 @@
 return {
   {
+    "romgrk/nvim-treesitter-context",
+    config = function()
+      require("treesitter-context").setup({
+        min_window_height = 50,
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        throttle = true, -- Throttles plugin updates (may improve performance)
+        max_lines = 10, -- How many lines the window should span. Values <= 0 mean no limit.
+        patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+          -- For all filetypes
+          -- Note that setting an entry here replaces all other patterns for this entry.
+          -- By setting the 'default' entry below, you can control which nodes you want to
+          -- appear in the context window.
+          default = {
+            "class",
+            "function",
+            "method",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/playground",
+    cmd = "TSHighlightCapturesUnderCursor",
+  },
+
+  {
     "nfrid/markdown-togglecheck",
     dependencies = { "nfrid/treesitter-utils" },
     ft = { "markdown", "qmd" },
