aac7b6bdb60a6dd2e461c1d7e524ee3276aaa591
Sam Foreman <saforem2@gmail.com>
2024-12-24 14:56:46 -0600

    feat: Add `lua/plugins/colors.lua`

---
+++ lua/plugins/colors.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "cyberdream",
+    },
+  },
+  {
+    "zootedb0t/citruszest.nvim",
+    lazy = false,
+    priority = 1000,
+    enabled = true,
+  },
+}
